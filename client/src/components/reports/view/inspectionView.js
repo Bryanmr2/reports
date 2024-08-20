6ed95acf23b1f829b601a52fa043c9f8260e03da
Bryan Mendoza <97215818+Bryanmr2@users.noreply.github.com>
2024-08-19 19:04:09 -0700

    "."

--- client/src/components/reports/view/inspectionView.js
+++ client/src/components/reports/view/inspectionView.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   Button,
-  Select,
-  InputLabel,
-  TextField,
   Table,
   TableBody,
   TableCell,
@@ -18,17 +15,13 @@ import { Link } from "react-router-dom";
 import "./inspectionView.css";
 
 const InspectionView = () => {
-  const [selectedUser, setSelectedUser] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
   const [reports, setReports] = useState([]);
-  // const [filterID, setFilterID] = useState("");
 
   const handleFetchReports = () => {
     axios
       .get("/api/inspections")
       .then((response) => {
-        setReports(response.data);
+        setReports(response.data); // Asegúrate de que response.data es un array de objetos
       })
       .catch((error) => {
         console.error("Error fetching inspections:", error);
@@ -39,71 +32,8 @@ const InspectionView = () => {
     handleFetchReports();
   }, []);
 
-  // const filteredReports = reports.filter((report) =>
-  //   report.id.toString().includes(filterID)
-  // );
-
   return (
     <div>
-      {/* <h2 style={{ marginLeft: "10px" }}>Consultar Inspecciones</h2>
-      <div>
-        <div className="view-container">
-          <Box>
-            <InputLabel htmlFor="userSelect">Seleccionar Usuario:</InputLabel>
-            <Select
-              id="userSelect"
-              value={selectedUser}
-              onChange={(e) => setSelectedUser(e.target.value)}
-              fullWidth
-            ></Select>
-          </Box>
-
-          <Box sx={{ marginTop: "20px" }}>
-            <InputLabel htmlFor="startDate">Fecha de inicio:</InputLabel>
-            <TextField
-              type="date"
-              id="startDate"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-              fullWidth
-            />
-          </Box>
-
-          <Box sx={{ marginTop: "20px" }}>
-            <InputLabel htmlFor="endDate">Fecha de finalización:</InputLabel>
-            <TextField
-              type="date"
-              id="endDate"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-              fullWidth
-            />
-          </Box>
-
-          <Box className="inspect-filter">
-            <InputLabel htmlFor="inspection-filter">
-              Filtrar por ID:{" "}
-            </InputLabel>
-            <TextField
-              label="0"
-              onChange={(e) => setFilterID(e.target.value)}
-              fullWidth
-            />
-          </Box>
-
-          <Box>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleFetchReports}
-              fullWidth
-              style={{ marginTop: "70px" }}
-            >
-              Consultar
-            </Button>
-          </Box>
-        </div> */}
-
       <Box sx={{ marginTop: "40px", padding: "1%" }}>
         <div
           style={{
@@ -127,10 +57,10 @@ const InspectionView = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {filteredReports.map((report) => (
+              {reports.map((report) => (
                 <TableRow key={report.id}>
                   <TableCell>{report.id}</TableCell>
-                  <TableCell>{report.title}</TableCell>
+                  <TableCell>{report.name}</TableCell>
                   <TableCell>{report.date}</TableCell>
                 </TableRow>
               ))}
@@ -139,7 +69,6 @@ const InspectionView = () => {
         </TableContainer>
       </Box>
     </div>
-    // </div>
   );
 };
 
